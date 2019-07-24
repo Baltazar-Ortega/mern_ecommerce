86e18f791531cf37275150f3b84845643cbb800a
Baltazar-Ortega} <kalbertog121@gmail.com>
2019-07-24 16:39:09 -0500

    production

--- ecommerce/routes/order.js
+++ ecommerce/routes/order.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-const { requiredSignin, isAuth } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
-const { create } = require("../controllers/order");
+const { requiredSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById, addOrderToUserHistory } = require("../controllers/user");
+const {
+  create,
+  listOrders,
+  getStatusValues,
+  updateOrderStatus,
+  orderById
+} = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
-router.post("/order/create/:userId", requiredSignin, isAuth, create);
+router.post(
+  "/order/create/:userId",
+  requiredSignin,
+  isAuth,
+  addOrderToUserHistory,
+  decreaseQuantity,
+  create
+);
+
+router.get("/order/list/:userId", requiredSignin, isAuth, isAdmin, listOrders);
+router.get(
+  "/order/status-values/:userId",
+  requiredSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+router.put(
+  "/order/:orderId/status/:userId",
+  requiredSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
 
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { requiredSignin, isAdmin, isAuth } = require("../controllers/auth");
-const { userById, read, update } = require("../controllers/user");
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory
+} = require("../controllers/user");
 
 router.get("/secret/:userId", requiredSignin, isAuth, isAdmin, (req, res) => {
   res.json({
@@ -12,6 +17,7 @@ router.get("/secret/:userId", requiredSignin, isAuth, isAdmin, (req, res) => {
 
 router.get("/user/:userId", requiredSignin, isAuth, read);
 router.put("/user/:userId", requiredSignin, isAuth, update);
+router.get("/orders/by/user/:userId", requiredSignin, isAuth, purchaseHistory);
 
 // Siempre que exista este parametro en la url, se ejecuta
 router.param("userId", userById);
